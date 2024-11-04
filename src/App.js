460df690a996c19701381cee4e70a9d48c3ac36b
Joseph Armstrong <Joeagorn@gmail.com>
2024-11-03 18:50:59 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import supabase from "./supabase";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("test_table_1").select("*");
+      setData(data);
+      setError(error);
+    };
+
+    // console.log(supabase);
+    fetchData();
+  }, []);
+
+  console.log(data);
+  console.log(error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Learn React + Supabase</header>
+      <div>
+        {error && <p>Error: {error.message}</p>}
+        {data ? (
+          <ul>
+            {data?.map((item) => (
+              <li key={item.id}>
+                {item.name} : {item.rating} Stars
+              </li> // Adjust according to your data structure
+            ))}
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 }
